2af6be70da2a18f9796038a792aa6ba49074ba2a
lemon <mauro.limarg@gmail.com>
2021-01-09 14:32:19 -0300

    adding action cp ly-config to /etc/ly/config.ini

--- ly-setup.sh
+++ ly-setup.sh
@@ -5,3 +5,4 @@ sudo make install
 # disable the current display manager
 sudo systemctl disable display-manager.service
 sudo systemctl enable ly.service
+sudo cp -fb ./ly-config.ini /etc/ly/config.ini
